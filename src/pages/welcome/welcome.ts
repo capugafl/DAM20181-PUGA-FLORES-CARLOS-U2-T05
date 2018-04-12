adf8f95d42603634dd3d4a65f392697680d0e83b
capugafl <capugafl@ittepic.edu.mx>
2018-04-11 20:18:28 -0600

    Tarea 5 - Unidad 2 - Aplicaciones Multiplataforma

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { UserService } from './../../services/user';
+import { MyComponent } from './../../components/foo';
+import { HomePage } from './../home/home';
+
+
+
+
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html',
+})
+
+export class WelcomePage {
+  user:any={
+    curp:'',
+  }
+
+ curp:string;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userSrv:UserService) {
+      console.log(`CURP: ${this.userSrv.curp}`);
+      this.user.curp=this.userSrv.curp;
+      this.curp=this.userSrv.curp;
+      this.curp=this.curp.substring(10,11);
+
+
+  }
+
+ ionViewDidLoad() {
+    console.log('ionViewDidLoad WelcomePage');
+  }
+
+  User():void{
+
+      this.navCtrl.push(HomePage);
+
+  }
+
+}
